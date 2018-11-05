c6fa56079469a3b6d4d82d1f4c7fc5ad563e81e4
Tim Cassell <tim.cassell@universalfieldrobots.com.au>
2018-11-06 07:59:08 +1000

    beam detector

---
+++ build/CMakeFiles/beam_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beam_detector.dir/src/beam_detector.cpp.o"
+  "beam_detector.pdb"
+  "beam_detector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/beam_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/beam_detector.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+  return (viewer);
+}
+
+
+int main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  //load point cloud
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/ufr/pcl_beam_detection/src/PCD/beam_1.pcd", *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file beam_1.pcd \n");
+    return (-1);
+  }
+
+  std::cerr << "Point cloud data: " << cloud->points.size () << " points" << std::endl;
+
+
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  seg.setInputCloud (cloud);
+  seg.segment (*inliers, *coefficients);
+
+  if (inliers->indices.size () == 0)
+  {
+    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+    return (-1);
+  }
+
+  std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                      << coefficients->values[1] << " "
+                                      << coefficients->values[2] << " " 
+                                      << coefficients->values[3] << std::endl;
+
+  std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
+  for (size_t i = 0; i < inliers->indices.size (); ++i)
+    std::cerr << inliers->indices[i] << "    " << cloud->points[inliers->indices[i]].x << " "
+                                               << cloud->points[inliers->indices[i]].y << " "
+                                               << cloud->points[inliers->indices[i]].z << std::endl;
+
+  return (0);
+}
+
