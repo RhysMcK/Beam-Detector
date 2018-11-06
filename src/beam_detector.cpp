acd834fea0ea8fa6f52b42f1b70b72512e921745
RhysMcK <rhysmckercher@gmail.com>
2018-11-06 17:39:13 +1000

    added visusaliser

--- src/beam_detector.cpp
+++ src/beam_detector.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cstdlib>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 
 boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
@@ -24,9 +26,11 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<
 
 int main (int argc, char** argv)
 {
+
+  std::string user_name(std::getenv("USER"));   //get computer user name for file path below
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
   //load point cloud
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/ufr/pcl_beam_detection/src/PCD/beam_1.pcd", *cloud) == -1) //* load the file
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/"+user_name+"/Beam-Detector/src/PCD/beam_2.pcd", *cloud) == -1) //* load the file
   {
     PCL_ERROR ("Couldn't read file beam_1.pcd \n");
     return (-1);
@@ -39,9 +43,7 @@ int main (int argc, char** argv)
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
   // Create the segmentation object
   pcl::SACSegmentation<pcl::PointXYZ> seg;
-  // Optional
   seg.setOptimizeCoefficients (true);
-  // Mandatory
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setDistanceThreshold (0.01);
@@ -55,17 +57,20 @@ int main (int argc, char** argv)
     return (-1);
   }
 
-  std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
-                                      << coefficients->values[1] << " "
-                                      << coefficients->values[2] << " " 
-                                      << coefficients->values[3] << std::endl;
+  // Extract the inliers
+  extract.setInputCloud (cloud);
+  extract.setIndices (inliers);
+  extract.setNegative (false);
+  extract.filter (*cloud_p);
 
-  std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
-  for (size_t i = 0; i < inliers->indices.size (); ++i)
-    std::cerr << inliers->indices[i] << "    " << cloud->points[inliers->indices[i]].x << " "
-                                               << cloud->points[inliers->indices[i]].y << " "
-                                               << cloud->points[inliers->indices[i]].z << std::endl;
-
+  //View pointcloud
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  viewer = simpleVis(cloud);
+  while (!viewer->wasStopped ())
+  {
+    viewer->spinOnce (100);
+    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+  }
   return (0);
 }
 
